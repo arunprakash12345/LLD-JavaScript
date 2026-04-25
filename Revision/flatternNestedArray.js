a5c998b0e696556108357849de00f2483b372a51
arunprakash12345 <arunprakash74119@gmail.com>
2026-04-25 22:08:06 +0530

    Add implementation of flattening functions for nested arrays using recursion and reduce method

---
+++ Revision/flatternNestedArray.js
@@ -0,0 +1,26 @@
+const arr = [1, [2, 3], [4, [5, 6], 7]];
+
+function flatternArray(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      result.push(...flatternArray(arr[i]));
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+
+console.log(flatternArray(arr));
+
+// Reduce method
+function flattenArrayReduce(arr) {
+  return arr.reduce((flattendedArray, item) => {
+    return flattendedArray.concat(
+      Array.isArray(item) ? flattenArrayReduce(item) : item,
+    );
+  }, []);
+}
+
+console.log(flattenArrayReduce(arr));
